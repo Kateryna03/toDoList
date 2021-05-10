5f363418059a37e6bd1479e67318cd6983ac15cd
Kateryna Gurienkova <gurienkova@gmail.com>
2021-05-10 11:13:41 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,23 @@
-// Напиши скрипт, который выполнит следующие операции.
+// Посчитает и выведет в консоль количество категорий в ul#categories, //то есть элементов li.item. Получится 'В списке 3 категории.'.
 
-// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
+const categoriesItemEl = document.querySelectorAll(".item");
+console.log(`В списке ${categoriesItemEl.length} категории.`);
 
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li)
 
-// Например для первой категории получится:
+categoriesItemEl.forEach((el) =>
+  console.log(
+    `Категория: ${el.firstElementChild.textContent}\nКоличество элементов: ${el.children[1].children.length};`
+  )
+);
+
+// const numberCategoryEl = [...categoriesItemEl]
+//   .map(
+//     (categories) => `Категория: ${categories.children[0].textContent}
+// Количество элементов: ${categories.children[1].children.length}`
+//   )
+//   .join("\n");
+// console.log(numberCategoryEl);
 
 // Категория: Животные
-// Количество элементов: 4
-console.log(document);
-const itemEl = document.querySelectorAll('.item');
-console.log(itemEl);
+// Количество элементов: 4;
