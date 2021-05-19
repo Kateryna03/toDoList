4b93f14e065aa80fb14388535dd58259e99e0b47
Kateryna Gurienkova <gurienkova@gmail.com>
2021-05-19 12:21:12 +0300

    08-task

--- js/task-08.js
+++ js/task-08.js
@@ -8,3 +8,21 @@
 // Размеры самого первого div - 30px на 30px
 // Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 // Создай функцию destroyBoxes(), которая очищает div#boxes.
+const boxesEl = document.querySelector("#boxes");
+console.log(boxesEl);
+const renderEl = document.querySelector('[data-action="render"]');
+console.log(renderEl);
+const destroyEl = document.querySelector('[data-action="destroy"]');
+console.log(destroyEl);
+
+renderEl.addEventListener("click", onCreateBoxes);
+destroyEl.addEventListener("click", onDestroyBoxes);
+
+function onCreateBoxes(amount) {
+  console.log(amount.currentTarget.value);
+  boxesEl.textContent = amount.currentTarget.value;
+}
+function onDestroyBoxes(amount) {
+  console.log(amount.currentTarget.value);
+  boxesEl.value = amount.currentTarget.value;
+}
