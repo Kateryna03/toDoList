cb43b5c91fa9d30e83c3a89c31445d907333f477
Kateryna Gurienkova <gurienkova@gmail.com>
2021-12-06 21:54:12 +0200

    todo

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,8 +1,6 @@
 const newTodoRef = document.querySelector(".message");
 const addToDoBtnRef = document.querySelector(".add");
-const deleteToDo = document.querySelector(".deleteButton");
 const toDoListRef = document.querySelector(".todo");
-console.log(newTodoRef, addToDoBtnRef, deleteToDo);
 
 //codewars task
 // function spinWords(string) {
@@ -61,16 +59,18 @@ function addTaskToDisplay() {
         <li>
         <input type= 'checkbox' id='item_${idx}' ${
       item.checked ? "checked" : ""
-    }>
+    }> </li> 
         <label for='item_${idx}' class="${
       item.important ? "important" : ""
     } ">${item.todo}</label>
-    <button onclick="deleteTask(${idx})" class="deleteButton"> Delete</button>
-        </li>
-      `;
+    
+    <button onclick='editTask(${idx})' class='deleteButton'>Edit</button>
 
-    toDoListRef.innerHTML = displayTask;
+    <button onclick='deleteTask(${idx})' class="deleteButton"> Delete</button>
+      
+      `;
   });
+  toDoListRef.innerHTML = displayTask;
 }
 
 // function onToDoChange(e) {
@@ -121,6 +121,62 @@ toDoListRef.addEventListener("change", function onToDoChange(e) {
 //   });
 // });
 
-const deleteTask = (index) => {
-  console.log(index);
-};
+function deleteTask(index) {
+  let todo = localStorage.getItem("todo");
+  toDoList = JSON.parse("todo");
+  toDoList.splice(index, 1);
+  localStorage.setItem("todo", JSON.stringify(toDoList));
+  addTaskToDisplay();
+}
+//задача на подсчет повторяюшихся эл в массиве
+// function likes(names) {
+//   // TODOfunction likes(names) {
+//   if (!names.length) {
+//     return "no one likes this";
+//   } else {
+//     for (item of names) {
+//       if (names.length === 1) {
+//         return `${names[0]} likes this`;
+//       }
+//       if (names.length === 2) {
+//         return `${names[0]} and ${names[1]} like this`;
+//       }
+//       if (names.length === 3) {
+//         return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+//       }
+//       if (names.length > 3) {
+//         rest = names.length - 2;
+//         return `${names[0]}, ${names[1]} and ${rest} others like this`;
+//       }
+//     }
+//   }
+
+//   //   let count = 0;
+//   //   names.forEach(name => count++);
+
+//   //   if(count === 1) {
+//   //     const firstName = names[0];
+//   //     return `${firstName} likes this`
+//   //   } else if (count == 2) {
+//   //     const firstName = names[0]
+//   //     const secondName = names[1]
+//   //     return `${firstName} and ${secondName} like this`
+//   //   }
+//   //   else if (count === 3) {
+//   //     const firstName = names[0]
+//   //     const secondName = names[1]
+//   //     const thirdName = names[2]
+//   //     return `${firstName}, ${secondName} and ${thirdName} like this`
+//   //   } else {
+//   //     const remainder = count - 2;
+//   //     const firstName = names[0]
+//   //     const secondName = names[1]
+//   //     return `${firstName}, ${secondName} and ${remainder} others like this`
+//   //   }
+// }
+
+// likes([]);
+// likes(["Peter"]);
+// likes(["Jacob", "Alex"]);
+// likes(["Max", "John", "Mark"]);
+// likes(["Alex", "Jacob", "Mark", "Max"]);
